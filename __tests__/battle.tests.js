c0b42f3ed15626150aeedbbdc6d567e64b6e5f1d
Justin Shaffer <justinbshaffer91@gmail.com>
2023-03-07 19:38:24 -0800

    test for keeping tract of players, PASS

--- __tests__/battle.tests.js
+++ __tests__/battle.tests.js
@@ -1,9 +1,21 @@
-const Battle = require('../src/battle');
-
 describe('Battle', () => {
   test('should initialize with an array of participants', () => {
     const participants = ['player1', 'player2', 'player3'];
     const battle = new Battle(participants);
     expect(battle.participants).toEqual(participants);
+  });
+
+  test('should generate a random turn order', () => {
+    const participants = ['player1', 'player2', 'player3'];
+    const battle = new Battle(participants);
+    expect(battle.turnOrder.length).toBe(participants.length);
+    expect(battle.turnOrder).not.toEqual(participants);
+  });
+
+  test('should keep the index of the currently active character in turnOrder', () => {
+    const participants = ['player1', 'player2', 'player3'];
+    const battle = new Battle(participants);
+    expect(battle.activeCharacterIndex).toBe(0);
+    expect(battle.turnOrder[battle.activeCharacterIndex]).toEqual(participants[0]);
   });
 });
--- src/battle.js
+++ src/battle.js
@@ -1,6 +1,14 @@
 class Battle {
   constructor(participants) {
     this.participants = participants;
+    this.turnOrder = [];
+    this.activeCharacterIndex = 0;
+    this.generateTurnOrder();
+  }
+
+  generateTurnOrder() {
+    const shuffledParticipants = this.participants.sort(() => Math.random() - 0.5);
+    this.turnOrder = [...shuffledParticipants];
   }
 }
 
