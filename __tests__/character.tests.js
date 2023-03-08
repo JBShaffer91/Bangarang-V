c8b05082791f3e6e3157f524059cc33c10e8fd37
Justin Shaffer <justinbshaffer91@gmail.com>
2023-03-07 18:43:20 -0800

    add tests for new Character, all PASS

--- __tests__/character.tests.js
+++ __tests__/character.tests.js
@@ -3,70 +3,58 @@ const Character = require('./../src/character');
 describe('Character', () => {
   test('should create a new character with the given name', () => {
     const character = new Character('Alice');
-    expect(character.name).toEqual('Alice');
+    expect(character.name).toBe('Alice');
   });
 
   test('should create a new character with default stats if none are given', () => {
     const character = new Character('Bob');
-    expect(character.strength).toEqual(10);
-    expect(character.dexterity).toEqual(10);
-    expect(character.constitution).toEqual(10);
-    expect(character.intelligence).toEqual(10);
-    expect(character.wisdom).toEqual(10);
-    expect(character.charisma).toEqual(10);
-    expect(character.health).toEqual(100);
-    expect(character.energy).toEqual(100);
-    expect(character.level).toEqual(1);
-    expect(character.luck).toEqual(10);
+    expect(character.strength).toBe(10);
+    expect(character.speed).toBe(10);
+    expect(character.health).toBe(100);
+    expect(character.charisma).toBe(10);
+    expect(character.intelligence).toBe(10);
+    expect(character.energy).toBe(100);
+    expect(character.level).toBe(1);
+    expect(character.luck).toBe(10);
   });
 
   test('should create a new character with the given strength', () => {
-    const character = new Character('Bob', { strength: 15 });
-    expect(character.strength).toEqual(15);
+    const character = new Character('Bob', 10);
+    expect(character.strength).toBe(10);
   });
 
-  test('should create a new character with the given dexterity', () => {
-    const character = new Character('Charlie', { dexterity: 20 });
-    expect(character.dexterity).toEqual(20);
-  });
-
-  test('should create a new character with the given constitution', () => {
-    const character = new Character('Dave', { constitution: 25 });
-    expect(character.constitution).toEqual(25);
-  });
-
-  test('should create a new character with the given intelligence', () => {
-    const character = new Character('Eve', { intelligence: 30 });
-    expect(character.intelligence).toEqual(30);
-  });
-
-  test('should create a new character with the given wisdom', () => {
-    const character = new Character('Frank', { wisdom: 35 });
-    expect(character.wisdom).toEqual(35);
-  });
-
-  test('should create a new character with the given charisma', () => {
-    const character = new Character('Grace', { charisma: 40 });
-    expect(character.charisma).toEqual(40);
+  test('should create a new character with the given speed', () => {
+    const character = new Character('Charlie', undefined, 10);
+    expect(character.speed).toBe(10);
   });
 
   test('should create a new character with the given health', () => {
-    const character = new Character('Henry', { health: 200 });
-    expect(character.health).toEqual(200);
+    const character = new Character('Eve', undefined, undefined, 100);
+    expect(character.health).toBe(100);
+  });
+
+  test('should create a new character with the given charisma', () => {
+    const character = new Character('Grace', undefined, undefined, undefined, 10);
+    expect(character.charisma).toBe(10);
+  });
+
+  test('should create a new character with the given intelligence', () => {
+    const character = new Character('Iris', undefined, undefined, undefined, undefined, 10);
+    expect(character.intelligence).toBe(10);
   });
 
   test('should create a new character with the given energy', () => {
-    const character = new Character('Iris', { energy: 150 });
-    expect(character.energy).toEqual(150);
+    const character = new Character('Jack', undefined, undefined, undefined, undefined, undefined, 100);
+    expect(character.energy).toBe(100);
   });
 
   test('should create a new character with the given level', () => {
-    const character = new Character('Jack', { level: 10 });
-    expect(character.level).toEqual(10);
+    const character = new Character('Kate', undefined, undefined, undefined, undefined, undefined, undefined, 1);
+    expect(character.level).toBe(1);
   });
 
   test('should create a new character with the given luck', () => {
-    const character = new Character('Kate', { luck: 50 });
-    expect(character.luck).toEqual(50);
+    const character = new Character('Lucas', undefined, undefined, undefined, undefined, undefined, undefined, undefined, 10);
+    expect(character.luck).toBe(10);
   });
 });
--- src/character.js
+++ src/character.js
@@ -1,11 +1,14 @@
 export class Character {
-  constructor(name, strength = 1, speed = 1, health = 100, charisma = 1, intelligence = 1, energy = 100, level = 1, luck = 1) {
+  constructor(name, { strength = 10, speed = 10, dexterity = 10, constitution = 10, intelligence = 10, wisdom = 10, charisma = 10, health = 100, energy = 100, level = 1, luck = 10 } = {}) {
     this.name = name;
     this.strength = strength;
     this.speed = speed;
-    this.health = health;
-    this.charisma = charisma;
+    this.dexterity = dexterity;
+    this.constitution = constitution;
     this.intelligence = intelligence;
+    this.wisdom = wisdom;
+    this.charisma = charisma;
+    this.health = health;
     this.energy = energy;
     this.level = level;
     this.luck = luck;
